028e7b1db05bfacdf080b65fda6d8cbc244068d7
luis12007 <00129020@edu.sv>
2023-04-04 12:55:25 -0600

    finished api :bookmark:

--- routes.js
+++ routes.js
@@ -1,14 +1,61 @@
-const { request } = require("express");
+const { request, response } = require("express");
+const jwt = require('jsonwebtoken');
 
 //manege requests with routes
 module.exports = function (app, databaseService) {
-    app.get('/Products', (req, response) => {
-        databaseService.GetProducts()
-            .then(products => {
-                response.json(products);
-            }).catch(e => {
-                response.status(500).json({ "mensaje": "peto unu" });
+
+
+    // creating a user token routes
+
+    app.post('/Login', (request, response) => {
+        const credentials = request.body;
+        const user = {
+            Nombre: "Belgiesv",
+            Pass: "Churros2023"
+        }
+        
+        if(credentials[0] == user[0] && credentials[1] == user[1]){
+            jwt.sign({ user: user }, 'secretkey',{expiresIn: '60m'}, (err, token) => {
+                response.json({
+                    token
+                })
             });
+        }else
+        response.status(500).json({ "mensaje": "credenciales incorrectas" });
+
+        
+    });
+
+    function verifyToken(request, response, next) {
+        const bearerHeader = request.headers['authorization'];
+
+
+        if (typeof bearerHeader !== 'undefined') {
+            const bearerToken = bearerHeader.split(" ")[1];
+            request.token = bearerToken;
+            next();
+        } else {
+            response.status(403).json({ "mensaje": "no viene el token" });
+
+        }
+    }
+
+
+    //Rutas de post
+
+    app.get('/Products', verifyToken, (req, response) => {
+        jwt.verify(req.token, 'secretkey', (error, authData) => {
+            if (error) {
+                response.status(403).json({ "mensaje": "no tienes token para hacer esta operacion" });
+            } else {
+                databaseService.GetProducts()
+                    .then(products => {
+                        response.json(products);
+                    }).catch(e => {
+                        response.status(500).json({ "mensaje": "peto unu" , authData});
+                    });
+            }
+        })
     });
 
 
@@ -57,5 +104,9 @@ module.exports = function (app, databaseService) {
                 response.status(500).json({ "mensaje": "peto unu" });
             });
     });
+
+
+
+    
 
 };
