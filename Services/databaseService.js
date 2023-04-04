6b6f04d1306a3524832cecd1f8af51b62a8dd5a2
luis12007 <00129020@edu.sv>
2023-04-04 11:41:10 -0600

    wow all CRUD ready

--- Services/databaseService.js
+++ Services/databaseService.js
@@ -7,7 +7,8 @@ const databaseService = () => {
             user : process.env.DB_USER,
             password : process.env.DB_PASS,
             database : process.env.DB,
-        }
+        },
+        pool: {min: 0, max:10},
     });
 
     const table = 'inventario';
@@ -15,6 +16,10 @@ const databaseService = () => {
     const GetProducts = () => {
         return knex(table).select();
     };
+
+    const FindProducts = (Params) => {
+        return knex(table).select().where({Nombre: Params});
+    };  
 
     const CreateProduct = ({Nombre, Cantidad, PrecioIndividual, PrecioCaja, DiaCompra, Caducidad}) => {
         return knex(table).insert({
@@ -27,7 +32,23 @@ const databaseService = () => {
         });
     };
 
-    return {GetProducts, CreateProduct};
+    const UpdateProduct = ({Nombre, Cantidad, PrecioIndividual, PrecioCaja, DiaCompra, Caducidad}, params) => {
+        return knex(table).where({idinventario: params}).update({
+            Nombre: Nombre,
+            Cantidad: Cantidad,
+            PrecioIndividual: PrecioIndividual,
+            PrecioCaja: PrecioCaja,
+            DiaCompra: DiaCompra,
+            Caducidad: Caducidad
+        });
+    };
+
+    const DeleteProduct = (params) => {
+        return knex(table).where({idinventario: params}).delete();
+    };
+
+
+    return {GetProducts, CreateProduct, FindProducts, UpdateProduct , DeleteProduct};
 };
 
 module.exports = {
--- routes.js
+++ routes.js
@@ -1,31 +1,61 @@
 const { request } = require("express");
 
 //manege requests with routes
-module.exports = function(app , databaseService){
-    app.get('/', (req, response) => {
-        response.json({"mensaje": "hola soy el main"});
-    });
-
-    app.get('/Products', (request,response) =>{
+module.exports = function (app, databaseService) {
+    app.get('/Products', (req, response) => {
         databaseService.GetProducts()
-        .then(products => {
-            response.json(products);
-        }).catch(e => {
-            response.status(500).json({"mensaje": "peto unu"});
-        });
+            .then(products => {
+                response.json(products);
+            }).catch(e => {
+                response.status(500).json({ "mensaje": "peto unu" });
+            });
     });
 
-    app.post('/Products', (request,response) =>{
+
+    app.get('/Products/:Nombre', (request, response) => {
+        const Nombre = request.params.Nombre;
+        databaseService.FindProducts(Nombre)
+            .then(products => {
+                response.json(products);
+            }).catch(e => {
+                response.status(500).json({ "mensaje": "peto unu" });
+            });
+    });
+
+    app.post('/Products', (request, response) => {
         const NewObject = request.body;
         console.log(NewObject);
 
         databaseService.CreateProduct(NewObject)
             .then(() => {
-                response.json({"mensaje": "Funciono agregado"});
+                response.json({ "mensaje": "Funciono agregado" });
             }).catch(e => {
-                response.status(500).json({"mensaje": "peto unu"});
+                response.status(500).json({ "mensaje": "peto unu" });
             });
-
-        
     });
+
+    app.put('/Products/:idinventario', (request, response) => {
+        const PutObject = request.body;
+        const params = request.params.idinventario;
+        console.log(PutObject);
+        console.log(params);
+
+        databaseService.UpdateProduct(PutObject, params)
+            .then(() => {
+                response.json({ "mensaje": "Funciono agregado" });
+            }).catch(e => {
+                response.status(500).json({ "mensaje": "peto unu" });
+            });
+    });
+
+    app.delete('/Products/:idinventario', (request, response) => {
+        const idinventario = request.params.idinventario;
+        databaseService.DeleteProduct(idinventario)
+            .then(products => {
+                response.json(products);
+            }).catch(e => {
+                response.status(500).json({ "mensaje": "peto unu" });
+            });
+    });
+
 };
